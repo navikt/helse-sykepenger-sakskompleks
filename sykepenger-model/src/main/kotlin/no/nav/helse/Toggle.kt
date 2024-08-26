4e75f704184472aa1da3cadd02c1334d8ed2be75
Hege Haavaldsen <hege.haavaldsen@nav.no>
2024-08-26 14:55:45 +0200

    wip

--- sykepenger-model/src/main/kotlin/no/nav/helse/Toggle.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/Toggle.kt
@@ -43,7 +43,7 @@ class Toggle private constructor(enabled: Boolean) {
     companion object {
         val TilkommenInntekt = fraEnv("TILKOMMEN_INNTEKT", false)
         val SendFeriepengeOppdrag = fraEnv("SEND_FERIEPENGEOPPDRAG", true)
-        val EgenmeldingStrekkerIkkeSykdomstidslinje = fraEnv("EGENMELDING_STREKKER_IKKE_SYKDOMSTIDSLINJE", false)
+        val EgenmeldingStrekkerIkkeSykdomstidslinje = fraEnv("EGENMELDING_STREKKER_IKKE_SYKDOMSTIDSLINJE", true)
 
         fun fraEnv(navn: String, defaultValue: Boolean) = Toggle(System.getenv(navn)?.lowercase()?.toBooleanStrictOrNull() ?: defaultValue)
 
